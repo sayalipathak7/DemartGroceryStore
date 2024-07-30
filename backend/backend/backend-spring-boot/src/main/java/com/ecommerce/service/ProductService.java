5eff23fe42228bf19111279b4ec8c4d4270dd55b
polagouni <shalinipolagouni21@gmail.com>
2024-07-29 23:11:23 -0500

    Update ProductService.java

--- backend/backend/backend-spring-boot/src/main/java/com/ecommerce/service/ProductService.java
+++ backend/backend/backend-spring-boot/src/main/java/com/ecommerce/service/ProductService.java
@@ -26,7 +26,6 @@ public interface ProductService {
 	
 	public List<Product> searchProduct(String query);
 	
-//	public List<Product> getAllProduct(List<String>colors,List<String>sizes,int minPrice, int maxPrice,int minDiscount, String category, String sort,int pageNumber, int pageSize);
 	public Page<Product> getAllProduct(String category, List<String>colors, List<String> sizes, Integer minPrice, Integer maxPrice, Integer minDiscount,String sort, String stock, Integer pageNumber, Integer pageSize, String brand, Integer rating);
 	
 	
