9bec398b0e14676a8192e1efc6282dca0e0f3c6e
sayalipathak7 <sayali7.pathak@gmail.com>
2024-08-01 14:31:29 -0500

    adding final frontend files

---
+++ frontend-main/frontend-main/frontend-react/src/customer/Components/Carousel/HomeCaroselData.js
@@ -0,0 +1,16 @@
+export const homeCarouselData=[
+    { 
+        image:"https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/general-nutrition-wellness/2-2-2-2foodgroups_vegetables_detailfeature.jpg?sfvrsn=226f1bc7_6",
+        path:"/Freshproduce/FruitsandVegetables/Vegetables"
+    },
+    {
+        image:"https://hips.hearstapps.com/hmg-prod/images/types-of-bread-1666723473.jpg?crop=1.00xw:0.757xh;0,0.0466xh&resize=1200:*",
+        path:"/DairyandBakery/dairyBakery/bread"
+    },
+    {
+        image:"https://thefriendlyeco.co.uk/cdn/shop/articles/8_73749738-2003-47e4-b561-63a3bb788a6f.png?v=1660571431&width=1100",
+        path:"/PersonalCare/products/SST"
+    },
+    
+]
+
